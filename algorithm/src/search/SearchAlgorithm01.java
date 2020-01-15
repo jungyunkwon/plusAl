d40a120b97e9125c20dd7c36489cdd4253e42ec0
Daniel5629 <apxls34@gmail.com>
2020-01-15 21:56:51 +0900

    3-1 예제

--- algorithm/src/search/SearchAlgorithm01.java
+++ algorithm/src/search/SearchAlgorithm01.java
@@ -16,23 +16,23 @@ public class SearchAlgorithm01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("요솟수:");
+        System.out.print("요솟수:");
         int num = sc.nextInt();
         int[] x = new int[num]; // 요소수가 입력받은 값인 배열
 
         for (int i = 0; i < num; i++) {
-            System.out.println("x["+ i +"]:");
+            System.out.print("x["+ i +"]:");
             x[i] = sc.nextInt();
         }
 
-        System.out.println("검색할 값:"); // 키값 입력
+        System.out.print("검색할 값:"); // 키값 입력
         int ky = sc.nextInt();
         int idx = seqSearch(x, num, ky); // 배열 x에서 키값이 ky인 요소 검색
 
         if (idx == -1) {
-            System.out.println("입력하신 값의 요소가 없습니다.");
+            System.out.print("입력하신 값의 요소가 없습니다.");
         }else {
-            System.out.println(ky+"은(는) x["+ idx +"]에 있습니다.");
+            System.out.print(ky+"은(는) x["+ idx +"]에 있습니다.");
         }
 
     }
