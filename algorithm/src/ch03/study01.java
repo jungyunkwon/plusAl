cd7e9f823b06cb414d63b32e2d6449a4659cc967
jungyunkwon <bayun_style@naver.com>
2020-01-16 21:20:31 +0900

    q2

--- algorithm/src/ch03/study01.java
+++ algorithm/src/ch03/study01.java
@@ -1,5 +1,24 @@
 package ch03;
 
 public class study01 {
-
+	public void q2() {
+int arr[]= {6,4,3,2,1,9,8,3};
+int key=3;
+String starbg=" |*";
+String star="\\* ";
+System.out.println("    |0 1 2 3 4 5 6");
+System.out.println("----|---------------------");
+for(int i=0;i<arr.length;i++) {
+	//별 찍어주기
+	starbg=starbg.replaceAll("\\*", "")+star;
+	System.out.println("   "+starbg);
+	System.out.println("   "+i+"|6 4 3 2 1 9 8");
+	//검색으로 찾아주기
+	if(arr[i]==key) {
+		System.out.println(key+"은 x["+i+"]에 있습니다.");
+			break;
+		
+	}
+}
+}
 }
---
+++ algorithm/src/main/run.java
@@ -0,0 +1,13 @@
+package main;
+
+import ch03.study01;
+
+public class run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		study01 s=new study01();
+		s.q2();
+	}
+
+}
